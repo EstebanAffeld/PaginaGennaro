060f26532ade7249c98ffcae594ae3e8a0ff49fb
Esteban Affeld <esteban.affeld@gmail.com>
2022-03-20 23:19:53 -0300

    subir mi primera pagina

---
+++ genaro/js/index.js
@@ -0,0 +1,10 @@
+const saludo = 'Hola mundo';
+const user = 'Genaro';
+
+document.getElementById('nameUser').innerHTML = user;
+
+console.log(saludo);
+
+// if (condition) {
+//     elemento x haga alguna accion , cambie de color
+// }
